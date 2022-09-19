62a3a607007ffa0713de18da7c22039eaa219b33
richard-xx <richard@arducam.com>
2022-09-19 15:07:14 +0800

    💚 divide files by architecture

--- build.sh
+++ build.sh
@@ -14,51 +14,52 @@ depthai_core() {
     dir_name="depthai-core"
     pkg_name="depthai"
     
-    printf "${GREEN}Clone %s ...${NC}\n" "${dir_name}"
+    echo -e "${GREEN}Clone ${dir_name} ...${NC}"
     cd /tmp || exit
     git clone --recursive https://github.com/luxonis/depthai-core.git --depth 1 -b ros-release 
 
     cd depthai-core || exit
 
     if [ "${ROS_DISTRO}" = "kinetic" ]; then
-        printf "${GREEN}Patch for %s ...${NC}\n" "${codename}"
+        echo -e "${GREEN}Patch for %s ...${NC}" "${codename}"
         git apply /workdir/xenial_package.diff
     fi
 
-    printf "${GREEN}Delete vcs files ...${NC}\n"
+    echo -e "${GREEN}Delete vcs files ...${NC}"
     rm -rf ./.git
     rm -rf ./*/*/.git
 
     version="$(sed -n '/<version>/p' package.xml | sed -E 's|.*>([0-9.].*)<.*|\1|g')"
     ros_pkg_name=ros-"${ROS_DISTRO}"-"${pkg_name}"_"${version}"
 
-    printf "${GREEN}Build %s ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} ...${NC}"
 
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
         orig_tar="${ros_pkg_name}.orig.tar.xz"
-        printf "${GREEN}Create %s ...${NC}\n" "${orig_tar}"
+        echo -e "${GREEN}Create ${orig_tar} ...${NC}"
         tar --xz -cf ../"${orig_tar}" "../${dir_name}"
     fi
 
-    printf "${GREEN}Create debian files ...${NC}\n"
+    echo -e "${GREEN}Create debian files ...${NC}"
     bloom-generate rosdebian -i "${debian_inc}"
     sed -i -e "s/(${version}-${debian_inc}${codename})/(${version}-${debian_inc}${codename}~${datetime})/g" debian/changelog
     cp /workdir/postinst_depthai_core debian/postinst
     
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
-        printf "${GREEN}Build %s source package ...${NC}\n" "${pkg_name}"
+        echo -e "${GREEN}Build ${pkg_name} source package ...${NC}"
         dpkg-buildpackage -S -j"$(nproc)"
     fi
 
-    printf "${GREEN}Build %s binary package ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} binary package ...${NC}"
     if [ "${ROS_DISTRO}" = "kinetic" ] || [ "${ROS_DISTRO}" = "melodic" ] || [ "${codename}" = "buster" ]; then
         env BUILD_TESTING_ARG="-DOpenCV_DIR=/home/ubuntu/OpenCV4.2/lib/cmake/opencv4" dpkg-buildpackage -b -j"$(nproc)"
     else
         dpkg-buildpackage -b -j"$(nproc)"
     fi
 
-    printf "${GREEN}Install %s deb ... ${NC}\n" "${ros_pkg_name}"
-    sudo apt install -qq -y ../${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb
+    echo -e "${GREEN}Install ${ros_pkg_name} deb ... ${NC}"
+    deb_name="${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb"
+    sudo apt install -qq -y ../${deb_name}
 
     sudo mkdir -p /workdir/artifacts/"${codename}"/
     sudo cp -u ../ros-"${ROS_DISTRO}"-* /workdir/artifacts/"${codename}"
@@ -68,7 +69,7 @@ foxglove_msgs() {
     dir_name="ros_foxglove_msgs"
     pkg_name="foxglove-msgs"
 
-    printf "${GREEN}Clone %s ...${NC}\n" "${dir_name}"
+    echo -e "${GREEN}Clone ${dir_name} ...${NC}"
     cd /tmp || exit
     git clone --recursive https://github.com/foxglove/schemas.git --depth 1 -b main
     cd schemas/"${dir_name}" || exit
@@ -76,34 +77,35 @@ foxglove_msgs() {
     version="$(sed -n '/<version>/p' package.xml | sed -E 's|.*>([0-9.].*)<.*|\1|g')"
     ros_pkg_name=ros-"${ROS_DISTRO}"-"${pkg_name}"_"${version}"
 
-    printf "${GREEN}Build %s ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} ...${NC}"
     
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
         orig_tar="${ros_pkg_name}.orig.tar.xz"
-        printf "${GREEN}Create %s ...${NC}\n" "${orig_tar}"
+        echo -e "${GREEN}Create ${orig_tar} ...${NC}"
         tar --xz -cf ../"${orig_tar}" "../${dir_name}"
     fi
 
-    printf "${GREEN}Create debian files ...${NC}\n"
+    echo -e "${GREEN}Create debian files ...${NC}"
     bloom-generate rosdebian -i ${debian_inc}
     sed -i -e "s/(${version}-${debian_inc}${codename})/(${version}-${debian_inc}${codename}~${datetime})/g" debian/changelog
 
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
-        printf "${GREEN}Build %s source package ...${NC}\n" "${ros_pkg_name}"
+        echo -e "${GREEN}Build ${pkg_name} source package ...${NC}"
         dpkg-buildpackage -S -j"$(nproc)"
     fi
 
-    printf "${GREEN}Build %s binary package ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} binary package ...${NC}"
     dpkg-buildpackage -b -j"$(nproc)"
 
-    printf "${GREEN}Install %s deb ... ${NC}\n" "${ros_pkg_name}"
-    sudo apt install -qq -y ../${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb
+    echo -e "${GREEN}Install ${ros_pkg_name} deb ... ${NC}"
+    deb_name="${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb"
+    sudo apt install -qq -y ../${deb_name}
 
     sudo mkdir -p /workdir/artifacts/"${codename}"/
     sudo cp -u ../ros-"${ROS_DISTRO}"-* /workdir/artifacts/"${codename}"
 }
 
-buildpackage() {
+build_package() {
     dir_name="$1"
     if [ -z "$2" ]; then
         pkg_name="$1"
@@ -116,34 +118,35 @@ buildpackage() {
         git clone --recursive https://github.com/luxonis/depthai-ros.git --depth 1 -b ros-release
     fi
 
-    printf "${GREEN}cd %s ...${NC}\n" "${dir_name}"
+    echo -e "${GREEN}cd ${dir_name} ...${NC}"
     cd depthai-ros/"${dir_name}" || exit
 
     version="$(sed -n '/<version>/p' package.xml | sed -E 's|.*>([0-9.].*)<.*|\1|g')"
     ros_pkg_name=ros-"${ROS_DISTRO}"-"${pkg_name}"_"${version}"
 
-    printf "${GREEN}Build %s ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} ...${NC}"
     
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
         orig_tar="${ros_pkg_name}.orig.tar.xz"
-        printf "${GREEN}Create %s ...${NC}\n" "${orig_tar}"
+        echo -e "${GREEN}Create ${orig_tar} ...${NC}"
         tar --xz -cf ../"${orig_tar}" "../${dir_name}"
     fi
 
-    printf "${GREEN}Create debian files ...${NC}\n"
+    echo -e "${GREEN}Create debian files ...${NC}"
     bloom-generate rosdebian -i "${debian_inc}"
     sed -i -e "s/(${version}-${debian_inc}${codename})/(${version}-${debian_inc}${codename}~${datetime})/g" debian/changelog
 
     if [ "$(dpkg --print-architecture)" = "amd64" ] && [ "${codename}" != "buster" ]; then
-        printf "${GREEN}Build %s source package ...${NC}\n" "${ros_pkg_name}"
+        echo -e "${GREEN}Build ${pkg_name} source package ...${NC}"
         dpkg-buildpackage -S -j"$(nproc)"
     fi
 
-    printf "${GREEN}Build %s binary package ...${NC}\n" "${ros_pkg_name}"
+    echo -e "${GREEN}Build ${ros_pkg_name} binary package ...${NC}"
     dpkg-buildpackage -b -j"$(nproc)"
 
-    printf "${GREEN}Install %s deb ... ${NC}\n" "${ros_pkg_name}"
-    sudo apt install -qq -y ../${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb
+    echo -e "${GREEN}Install ${ros_pkg_name} deb ... ${NC}"
+    deb_name="${ros_pkg_name}-${debian_inc}${codename}~${datetime}_$(dpkg --print-architecture).deb"
+    sudo apt install -qq -y ../${deb_name}
 
 }
 
@@ -162,19 +165,19 @@ while getopts "cfmbera" arg; do #第一个冒号表示忽略错误；字符后�
         foxglove_msgs
         ;;
     r)
-        buildpackage depthai_ros_msgs depthai-ros-msgs
-        buildpackage depthai_bridge depthai-bridge
-        buildpackage depthai_examples depthai-examples
-        buildpackage depthai-ros
+        build_package depthai_ros_msgs depthai-ros-msgs
+        build_package depthai_bridge depthai-bridge
+        build_package depthai_examples depthai-examples
+        build_package depthai-ros
         copy_artifacts
         ;;
     a)
         depthai_core
         foxglove_msgs
-        buildpackage depthai_ros_msgs depthai-ros-msgs
-        buildpackage depthai_bridge depthai-bridge
-        buildpackage depthai_examples depthai-examples
-        buildpackage depthai-ros
+        build_package depthai_ros_msgs depthai-ros-msgs
+        build_package depthai_bridge depthai-bridge
+        build_package depthai_examples depthai-examples
+        build_package depthai-ros
         copy_artifacts
         ;;
     *)
