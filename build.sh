2ca1835ae5a5ef5b9d07108592d2a593e79e74bd
richard-xx <99704599+richard-xx@users.noreply.github.com>
2023-04-27 13:57:15 +0800

    add -us -uc

--- build.sh
+++ build.sh
@@ -242,10 +242,10 @@ build_package() {
 
     if [[ "${package_type}" == "core" ]] || [[ "${ROS_DISTRO}" == "kinetic" ]] || [[ "${ROS_DISTRO}" == "melodic" ]] || [[ "${LINUX_DISTRO}" == "buster" ]]; then
         info "Building ${ros_pkg_name} binary package with BUILD_TESTING_ARG and OpenCV_DIR..."                                                               # 提示正在使用BUILD_TESTING_ARG和OpenCV_DIR构建二进制包
-        env DEB_BUILD_OPTIONS=noautodbgsym BUILD_TESTING_ARG="-DOpenCV_DIR=/home/ubuntu/OpenCV4.2/lib/cmake/opencv4" dpkg-buildpackage -b -j"$(nproc)" -Zgzip # 使用BUILD_TESTING_ARG和OpenCV_DIR构建二进制包
+        env DEB_BUILD_OPTIONS=noautodbgsym BUILD_TESTING_ARG="-DOpenCV_DIR=/home/ubuntu/OpenCV4.2/lib/cmake/opencv4" dpkg-buildpackage -b -us -uc -j"$(nproc)" -Zgzip # 使用BUILD_TESTING_ARG和OpenCV_DIR构建二进制包
     else
         info "Building ${ros_pkg_name} binary package..."                    # 提示正在构建二进制包
-        env DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -b -j"$(nproc)" # 构建二进制包
+        env DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -b -us -uc -j"$(nproc)" -Zgzip # 构建二进制包
     fi
 
     deb_name="${ros_pkg_name}-${DEBSUFFIX}"         # Debian包名称
